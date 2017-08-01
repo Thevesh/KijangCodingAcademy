005a2000a3c7949ef8bbcc5544e5e0707cbc1c6e
Thevesh <thevesh.theva@gmail.com>
2017-08-01 16:24:49 +0800

    Improved Roulette performance

--- src/project_Roulette.java
+++ src/project_Roulette.java
@@ -18,8 +18,9 @@ public class project_Roulette {
 			
 			int wallet = 1000;
 			int tries = 1;
+			int response = 1;
 			
-			while (wallet > 0){
+			while (wallet > 0 && response == 1){
 				
 				int house = generate.nextInt(10000);
 
@@ -28,6 +29,12 @@ public class project_Roulette {
 				System.out.println("There is a pot of $" + house + " on the table.");
 				System.out.println("How much would you like to bet?");
 				int bet = kijang.nextInt();
+				
+				while (bet > wallet){
+					System.out.println("Don't be daft. We're an ethical casino. We don't let people spend money they don't have.");
+					System.out.println("How much would you like to bet?");
+					bet = kijang.nextInt();
+				}
 				
 				if (bet == wallet){
 					System.out.println("Wow! All in! A high roller!");
@@ -42,6 +49,12 @@ public class project_Roulette {
 				System.out.println("Nice. Now, pick a number between 1 and 38.");
 				int hope = kijang.nextInt();
 				
+				while (hope < 1 || hope > 38){
+					System.out.println("Between 1 and 38, yo. Are you sure you're smart enough for this?");
+					System.out.println("For real this time - What number are you betting on?");
+					hope = kijang.nextInt();
+				}
+				
 				int result;
 				
 				if (tries%4 == 0) {
@@ -52,7 +65,7 @@ public class project_Roulette {
 				
 				System.out.println("The wheel has been spun. There's no turning back now.");
 				System.out.println("Wait for it.....");
-				TimeUnit.SECONDS.sleep(5);
+				TimeUnit.SECONDS.sleep(4);
 				
 
 				System.out.println("");
@@ -61,9 +74,14 @@ public class project_Roulette {
 				if (hope == result){
 					System.out.println("You've won $" + house + "!!!! What a stunning victory!");
 					wallet += house;
-					System.out.println("Would you like to try your luck again while you're on a streak? :D");
-					int response = kijang.nextInt();
 					
+					System.out.println("Would you like to try your luck again while you're on a streak? :D  (yes / no)");
+					response = ResponseHandler(kijang.next());
+					
+					while (response == 2){
+						System.out.println("Yes or no. Is that so difficult?");
+						response = ResponseHandler(kijang.next());
+					}
 					if (response == 0) {
 						System.out.println("Thank you for playing. See you next time! You leave with $" + wallet + ".");
 						break;
@@ -77,9 +95,15 @@ public class project_Roulette {
 					if (wallet == 0) System.out.println("Unfortunately, you're out of money. You're gonna have to leave for now, but do come back again! I just know you're meant to make it here!");
 					
 					else{
-					System.out.println("Would you like to try your luck again? I'm sure that was a one-off!");
-					int response = kijang.nextInt();
+						
+						System.out.println("Would you like to try your luck again? I'm sure that was a one-off!  (yes / no)");
+						response = ResponseHandler(kijang.next());
 					
+						while (response == 2){
+							System.out.println("Just answer 'Yes' or 'No'. Is that so difficult?");
+							response = ResponseHandler(kijang.next());
+						}
+						
 						if (response == 0) {
 							System.out.println("Thank you for playing. See you next time! You leave with $" + wallet + ".");
 							break;
@@ -93,6 +117,12 @@ public class project_Roulette {
 			
 			
 		}
+	
+	public static int ResponseHandler (String n){
+		if (n.equalsIgnoreCase("yes")) return 1;
+		else if (n.equalsIgnoreCase("no")) return 0;
+		else return 2;
+	}
 }
 
 
